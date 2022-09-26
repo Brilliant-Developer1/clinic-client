52f22c38e863ec2eb61e2a73a906149371774925
Obaidullah <86623417+Brilliant-Developer1@users.noreply.github.com>
2022-09-27 00:47:51 +0600

    contact edit

--- src/Pages/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -11,26 +11,28 @@ const ContactUs = () => {
         backgroundImage: `url(${background})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
-        height: 500,
+        height: 550,
         width: '100%',
       }}
     >
-      <div className=" text-base-100">
+      <div className="mt-12">
         <h1 className=" text-center  text-xl font-bold text-primary">
           CONTACT US
         </h1>
-        <h1 className="text-3xl text-center">Stay Connected with us</h1>
+        <h1 className="text-3xl text-center text-base-100">
+          Stay Connected with us
+        </h1>
         <div className="flex flex-col items-center">
           <div className="flex flex-col w-full">
             <input
               type="email"
               placeholder="Email Address"
-              className="input input-bordered input-accent text-black mt-10 inputs"
+              className="input input-bordered input-accent  mt-10 inputs"
             />
             <input
               type="text"
               placeholder="Subject"
-              className="input input-bordered input-accent  text-black mt-5 inputs"
+              className="input input-bordered input-accent   mt-5 inputs"
             />
             <textarea
               className="textarea textarea-accent mt-5 inputs h-32"
