3ba26fa0e3c1ae05b36b304856d609d94ce37426
Obaidullah <86623417+Brilliant-Developer1@users.noreply.github.com>
2022-11-01 20:38:57 +0600

    login form edited

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,38 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../../assets/icons/google.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  if (user) {
+    console.log(user);
+  }
+
   return (
     <div className=" container mx-auto">
       <div className="flex items-center justify-center mt-28 lg:mt-36">
         <div className="card  bg-base-100 shadow-xl">
           <div className="flex flex-col w-full  p-5">
             <h1 className="text-2xl text-center">Login</h1>
-            <input
-              type="email"
-              placeholder="Email Address"
-              className="input input-bordered input-accent mt-3 inputs"
-            />
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+              <input
+                {...register('email', { pattern: /[A-Za-z]{3}/ })}
+                type="email"
+                placeholder="Email Address"
+                className="input input-bordered input-accent mt-3 inputs"
+              />
+              {errors.email?.type === 'required' && 'Email is required'}
 
-            <input
-              type="password"
-              placeholder="Password"
-              className="input input-bordered input-accent   mt-3 inputs"
-            />
-            <a
-              href="http://localhost:3000/login"
-              className="text-left hover:text-cyan-500"
-            >
-              Forgot Password?
-            </a>
-            <button
-              type="button"
-              className="btn w-full border-none bg-gray-600 text-base-100  hover:bg-gray-500 shadow-lg hover:shadow-md mt-8 mb-5
+              <input
+                autoComplete="password"
+                type="password"
+                placeholder="Password"
+                className="input input-bordered input-accent   mt-3 inputs"
+              />
+              {errors.password?.type === 'required' && 'Password is required'}
+              <a
+                href="http://localhost:3000/login"
+                className="text-left hover:text-cyan-500"
+              >
+                Forgot Password?
+              </a>
+              <input
+                type="submit"
+                value="LOGIN"
+                className="btn w-full border-none bg-gray-600 text-base-100  hover:bg-gray-500 shadow-lg hover:shadow-md mt-8 mb-5
 "
-            >
-              Login
-            </button>
+              />
+            </form>
+
             <p className="text-center">
               New to Doctors Portal?{' '}
               <Link to="signup" className="text-accent hover:text-cyan-500">
@@ -44,6 +67,7 @@ const Login = () => {
               type="button"
               className="btn w-full bg-white text-primary hover:border-none  hover:bg-gray-100 shadow-lg hover:shadow-md mt-2 mb-4
 "
+              onClick={() => signInWithGoogle()}
             >
               CONTINUE WITH GOOGLE
               <div className="avatar ml-2">
